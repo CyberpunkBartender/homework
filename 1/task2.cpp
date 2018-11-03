ae33d6380d8a73673613bf01286106e8f4a1b395
Aleksandr Nuzhin <nuzhin-92@mail.ru>
2018-11-04 00:24:01 +0300

    task2

---
+++ 1/task2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a;
+	string b;
+	string c;
+	cin >> a >> b >> c;
+	if (a<b && a<c){
+		cout << a;
+	}
+	else{
+		if(b<a && b<c){
+			cout << b;
+	}
+	else{
+			cout << c;
+	}
+	}
+    return 0;
+}
