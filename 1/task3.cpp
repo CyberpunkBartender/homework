b912f677820c525116177342e42bb673786202d2
Aleksandr Nuzhin <nuzhin-92@mail.ru>
2018-11-04 00:43:20 +0300

    task3

---
+++ 1/task3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	double a,b,c,x;
+	cin a,b,c;
+	if ((b*b-4*a*c)>=0) {
+		x=(-1*b-sqrt(b*b-4*a*c))/(2*a);
+		cout << x << endl;
+		x=(-1*b+sqrt(b*b-4*a*c))/(2*a);
+		cout << x << endl;
+	}
+	else {
+		cout << "impossible" << endl;	}
+	}
